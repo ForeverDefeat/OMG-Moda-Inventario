293739ec0a9d04754ff068780bbd09cde834b68c
RodrigoMoron <rodrigomp.3008@gmail.com>
2026-07-09 00:38:05 -0500

    Allow actuator health endpoint

--- sistema-inventario/src/main/java/com/omgmoda/sistema_inventario/usuario/infraestructura/security/SecurityConfig.java
+++ sistema-inventario/src/main/java/com/omgmoda/sistema_inventario/usuario/infraestructura/security/SecurityConfig.java
@@ -71,6 +71,7 @@ public class SecurityConfig {
                             "/v3/api-docs",
                             "/v3/api-docs/**"
                     ).permitAll()
+                    .requestMatchers("/actuator/health").permitAll()
                     .requestMatchers("/uploads/**").permitAll()
 
                     // Login público
